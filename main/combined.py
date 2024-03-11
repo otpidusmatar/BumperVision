0e4b2fb15272208471c149d99de1528d97cb55ba
Aniket Chakraborty <139797826+otpidusmatar@users.noreply.github.com>
2024-03-11 14:43:08 -0500

    Implemented smooth optical flow and close key

--- main/combined.py
+++ main/combined.py
@@ -10,7 +10,7 @@ feature_params = dict(maxCorners = 600, qualityLevel = 0.2, minDistance = 2, blo
 # Parameters for Lucas-Kanade optical flow
 lk_params = dict(winSize = (15,15), maxLevel = 2, criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 # The video feed is read in as a VideoCapture object
-cap = cv.VideoCapture("main/testvideos/2022compvid.mp4")
+cap = cv.VideoCapture("main/testvideos/trimmed2022compvid.mp4")
 # Variable for color to draw optical flow track
 color = (0, 255, 0)
 # ret = a boolean return value from getting the frame, first_frame = the first frame in the entire video sequence
@@ -25,6 +25,20 @@ mask = np.zeros_like(first_frame)
 
 iteration = 1
 frame_retention = 15
+
+frame_memory = [mask]  # This is where the previous masked frames will be stored. The first frame is blank for future use, the memory length is frame_retention + 1
+# Initialize blank frames in frame memory
+for i in range(0, frame_retention-1):
+    i = np.zeros_like(first_frame)
+    frame_memory.append(i)
+
+def create_output_mask(masklist):
+    product = masklist[0]
+    for i in range(0, len(masklist)-1):
+        product = cv.add(product, masklist[i+1])
+    masklist = masklist.pop(1)
+    return product
+
 while(cap.isOpened()):
     # ret = a boolean return value from getting the frame, frame = the current frame being projected in the video
     ret, frame = cap.read()
@@ -48,18 +62,21 @@ while(cap.isOpened()):
         mask = cv.line(mask, (a, b), (c, d), color, 2)
         # Draws filled circle (thickness of -1) at new position with green color and radius of 3
         frame = cv.circle(frame, (a, b), 3, color, -1)
+    frame_memory.append(mask)
+    overlay_mask = create_output_mask(frame_memory)
     # Overlays the optical flow tracks on the original frame
-    output = cv.add(frame, mask)
+    output = cv.add(frame, overlay_mask)
     # Updates previous frame
     prev_gray = gray.copy()
     # Updates previous good feature points
     prev = good_new.reshape(-1, 1, 2)
     # Combines optical flow and bumper detection model and outputs display
     result = model.track(source=output, show=True)
-    # Removes optical flow markings every 2 frames
-    if iteration % frame_retention == 0:
-        mask = np.zeros_like(first_frame)
-    iteration += 1
+    # Resets mask to prevent carry-over from outside of what is stored in frame_memory
+    mask = np.zeros_like(first_frame)
+    # Frames are read by intervals of 10 milliseconds. The programs breaks out of the while loop when the user presses the 'q' key
+    if cv.waitKey(10) & 0xFF == ord('q'):
+        break
 # The following frees up resources and closes all windows
 cap.release()
 cv.destroyAllWindows()
