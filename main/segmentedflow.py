14859fcc9979e955bf3287d12067d1dda2170fac
Aniket Chakraborty <139797826+otpidusmatar@users.noreply.github.com>
2024-03-13 13:50:40 -0500

    Ran tests for higher maxcorner settings on testvid

--- main/segmentedflow.py
+++ main/segmentedflow.py
@@ -3,11 +3,11 @@ import numpy as np
 from bumpersegment import bumpersegment_preprocess
 
 # Parameters for Shi-Tomasi corner detection
-feature_params = dict(maxCorners = 300, qualityLevel = 0.2, minDistance = 2, blockSize = 7)
+feature_params = dict(maxCorners = 900, qualityLevel = 0.2, minDistance = 2, blockSize = 7)
 # Parameters for Lucas-Kanade optical flow
 lk_params = dict(winSize = (15,15), maxLevel = 2, criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 # The video feed is read in as a VideoCapture object
-cap = cv.VideoCapture("main/testvideos/trimmed2024robotrevealvid.mp4")
+cap = cv.VideoCapture("main/testvideos/testvid.mp4")
 # Variable for color to draw optical flow track
 color = (0, 255, 0)
 # ret = a boolean return value from getting the frame, first_frame = the first frame in the entire video sequence
