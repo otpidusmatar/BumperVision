aa86cd0b472d4ebd675d8adee96894dfd0c797d5
Aniket Chakraborty <139797826+otpidusmatar@users.noreply.github.com>
2024-03-14 13:16:19 -0500

    Tested unitary color filter on multi-robot video

--- main/segmentedflow.py
+++ main/segmentedflow.py
@@ -7,7 +7,7 @@ feature_params = dict(maxCorners = 300, qualityLevel = 0.2, minDistance = 5, blo
 # Parameters for Lucas-Kanade optical flow
 lk_params = dict(winSize = (15,15), maxLevel = 2, criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 # The video feed is read in as a VideoCapture object
-cap = cv.VideoCapture("main/testvideos/redvblue1vid.mp4")
+cap = cv.VideoCapture("main/testvideos/testvid.mp4")
 # Variable for color to draw optical flow track
 color = (0, 255, 0)
 # ret = a boolean return value from getting the frame, first_frame = the first frame in the entire video sequence
